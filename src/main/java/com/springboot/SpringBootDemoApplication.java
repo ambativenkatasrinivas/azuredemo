65e4cbc1f5f5cdc7370fea215ed85db753807025
ambativenkatasrinivas <ambati.srinivas45@gmail.com>
2017-07-24 20:55:24 +0530

    main class modified

--- src/main/java/com/springboot/SpringBootDemoApplication.java
+++ src/main/java/com/springboot/SpringBootDemoApplication.java
@@ -1,10 +1,7 @@
 package com.springboot;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,10 +13,10 @@ public class SpringBootDemoApplication {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 	
-	@Bean
+/*	@Bean
     ServletRegistrationBean h2servletRegistration(){
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
-    }
+    }*/
 }
