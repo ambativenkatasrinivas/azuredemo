e32c93c96f461317497ad0294b9118f41bf94349
ambativenkatasrinivas <ambati.srinivas45@gmail.com>
2017-07-24 23:15:44 +0530

    logging properties added

--- src/main/java/com/springboot/controller/EmployeeController.java
+++ src/main/java/com/springboot/controller/EmployeeController.java
@@ -3,6 +3,9 @@ package com.springboot.controller;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,20 +23,27 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	 private static Logger log = Logger.getLogger(EmployeeController.class);
+	
 	@RequestMapping(value= "/employee", method = RequestMethod.GET)
     public List<Employee> getEmployees() 
     {
+		log.info("getting list of employees");
 		return employeeService.getEmployees();
     }
 	
 	@RequestMapping(value="/employee/", method = RequestMethod.POST)
 	public ResponseEntity<?> createEmployee(@RequestBody Employee employee) {
+		log.info("creating new employee");
 		employeeService.createEmployee(employee);
 		return ResponseEntity.ok("Employee created.");
 	}
 	
 	@RequestMapping(value="/employee/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<?> updateEmployee(@RequestBody Employee employee) {
+	public ResponseEntity<?> updateEmployee(@PathParam("id") Long id, @RequestBody Employee employee) {
+		log.info("updating employee: " +id);
+		if(id == 1000)
+			throw new NullPointerException();
 		employeeService.updateEmployee(employee);
 		return ResponseEntity.ok("employee updated.");
 	}
